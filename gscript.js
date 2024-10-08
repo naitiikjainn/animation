42851f2ea48179f08733524d9be3f2047b7ad011
Naitik Jain <150283488+naitiikjainn@users.noreply.github.com>
2024-10-08 20:49:53 +0530

    files updated

--- gscript.js
+++ gscript.js
@@ -7,29 +7,33 @@ canvas.height = window.innerHeight;
 
 var stars = [];
 var numStars = 1000;
-var speed = parseInt(speedControl.value);
+//parseInt used for converting string to integer as the value of speedControl is string by default i have to change it to integer so i can change the speed of the stars
+//we can also use +speedControl.value to convert string to integer
+var speed = +speedControl.value;
 
 for (var i = 0; i < numStars; i++) {
     stars.push({
+        //Math.random() - 0.5 is used to get random number between -0.5 and 0.5
         x: (Math.random() - 0.5) * canvas.width,
         y: (Math.random() - 0.5) * canvas.height,
+        // between 0 and canvas width
         z: Math.random() * canvas.width
     });
 }
 
 function animate() {
+    //first we have to fill the entire canvas with black color
     c.fillStyle = 'black';
     c.fillRect(0, 0, canvas.width, canvas.height);
-
+    //stars with white color
     c.fillStyle = 'white';
     for (var i = 0; i < stars.length; i++) {
         var star = stars[i];
-
+        //
         var k = canvas.width / star.z;
         var x = star.x * k + canvas.width / 2;
         var y = star.y * k + canvas.height / 2;
         var r = 1 * k;
-
         c.beginPath();
         c.arc(x, y, r, 0, 2 * Math.PI);
         c.fill();
@@ -45,7 +49,7 @@ function animate() {
     requestAnimationFrame(animate);
 }
 
-speedControl.addEventListener('input', function() {
+speedControl.addEventListener('input',()=> {
     speed = parseInt(speedControl.value);
     
 });
